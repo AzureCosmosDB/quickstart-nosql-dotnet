16bc83f24d1bb39bacfd20680f2dd1455a18cb4a
Sidney Andrews <sidandrews@microsoft.com>
2024-09-05 22:28:16 +0000

    Initial commit

---
+++ src/console/Program.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+
+string endpoint = "";
+
+var credential = new DefaultAzureCredential();
+
+using CosmosClient client = new(endpoint, credential);
+
+Database database = client.GetDatabase("cosmicworks");
+Container container = database.GetContainer("products");
+
+Item newItem = new("0000", "Test item", "Test SKU");
+
+await container.UpsertItemAsync<Item>(newItem);
+
+record Item(string id, string name, string sku);
